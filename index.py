7bb68fdc5b871e65225580fb936578f73d8b7215
Andy Lim <limchinhuat0221@gmail.com>
2021-08-15 16:33:32 +0800

    add cfn_nag suppression reason in sample.yaml

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
 import boto3 
 import yaml 
+import logging 
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 client = boto3.client('cloudformation', region_name='us-east-1')
 
@@ -8,9 +12,22 @@ class CloudFormationStack:
         self.stack_name = json["StackName"]
         self.template_file = json["TemplateFile"]
         self.parameters = json["Parameters"]
+        self.capabilties = json["Capabilities"]
+        self.tags = json["Tag"] if json["Tag"] else []
 
     def deploy(self):
-        print(self.stack_name)
+        try:
+            response = client.create_stack(
+                            StackName=self.stack_name,
+                            TemplateBody=self.template_file,
+                            Parameters=self.parameters,
+                            Capabilities=self.capabilties,
+                            Tags=self.tags
+                        )
+            logging.info(response["StackId"])
+        except Exception as e:
+            logging.error(e)
+        
 
 if __name__ == "__main__":
     with open('manifest.yaml') as f:
