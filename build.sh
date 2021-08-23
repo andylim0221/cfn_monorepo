0794dbc06cc5488017ad65a4fc45d9d2db275f37
andylim0221 <limchinhuat0221@gmail.com>
2021-08-23 17:14:06 +0800

    try commit again

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-set -e
 echo "Accessing templates"
 
 cfn-lint templates/**/*.yaml
 EXIT_CODE=$?
 
-if [$EXIT_CODE == 4]; then 
-    EXIT_CODE=0
-fi 
+# Exit code 4 is warnings, which we don't want to error. See https://github.com/aws-cloudformation/cfn-python-lint/issues/235#issuecomment-406951041
+if [ $EXIT_CODE == 4 ]; then
+  EXIT_CODE=0
+fi
 
-exit $EXIT_CODE
+exit $EXIT_CODE 
--- manifest.py
+++ manifest.py
@@ -1,50 +1,48 @@
 import boto3 
 import yaml 
 import logging 
+import time 
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 client = boto3.client('cloudformation', region_name='us-east-1')
 
+CAPABILITIES = ['CAPABILITY_IAM','CAPABILITY_NAMED_IAM','CAPABILITY_AUTO_EXPAND']
+
 class CloudFormationStack:
     def __init__(self, json):
+
         self.client = client 
         self.stack_name = json["StackName"]
         self.template_file = json["TemplateFile"]
         self.parameters = json["Parameters"]
         self.capabilties = json["Capabilities"]
         self.region = json["Region"]
-        self.tags = json["Tags"] if json["Tags"] else []
+        self.tags = json["Tags"] if json["Tags"] else None 
 
-    def get_tags(self):
-        self.tags.extend([{
-            'DEPLOY_THROUGH': 'AZURE_DEVOPS'
-        }])
-
-    def deploy(self):
-        try:
-            response = client.describe_stacks(StackName=self.stack_name)
-        except Exception as e:
-            logging.info(f'${self.stack_name} does not exist')
-            logging.warn(e)
-
-            response = client.create_stack(
-                            StackName=self.stack_name,
-                            TemplateBody=self.template_file,
-                            Parameters=self.parameters,
-                            Capabilities=self.capabilties,
-                            Tags=self.tags
-                        )
-            logging.info(response["StackId"])
+    def get_stack(self, stack_name):
+        try: 
+            response = self.client.decribe_stacks(StackName=stack_name)
         except Exception as e:
             logging.error(e)
-        
+            return None
+        else:
+            return response["Stacks"][0]
+
+    def validate_template(self, **kwargs):
+        try:
+            self.client.validate_template(**kwargs)
+        except Exception as e:
+            logging.error(e)
+
+    def run(self):
+        self.validate_template(self.template_file)
 
 if __name__ == "__main__":
     with open('manifest.yaml') as f:
         stacks = yaml.safe_load(f)
-        print(stacks)
+        # print(stacks)
         for stack in stacks["Stacks"]:
             cfn = CloudFormationStack(stack)
-            cfn.deploy()
+            cfn.run()
