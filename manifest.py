6db069bcb6b4c29ca1c82ea2a621cf5ae6e3bccd
andylim0221 <limchinhuat0221@gmail.com>
2021-08-25 18:16:10 +0800

    remove empty cfn template

--- manifest.py
+++ manifest.py
@@ -7,10 +7,9 @@ import os
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-client = boto3.client('cloudformation', region_name='us-east-1')
+client = boto3.client("cloudformation", region_name="us-east-1")
 
-CAPABILITIES = ['CAPABILITY_IAM',
-                'CAPABILITY_NAMED_IAM', 'CAPABILITY_AUTO_EXPAND']
+CAPABILITIES = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
 
 """
 1. collect config from each stack
@@ -20,7 +19,7 @@ CAPABILITIES = ['CAPABILITY_IAM',
 
 def collect_stack_config(stack):
     response = {}
-    
+
     parameters = []
     if "Parameters" in stack:
         for parameter in stack["Parameters"]:
@@ -28,15 +27,20 @@ def collect_stack_config(stack):
                 parameters.append({"ParameterKey": key, "ParameterValue": str(value)})
     response["Parameters"] = parameters
 
-    response["EnableTerminationProtection"] = stack["EnableTerminationProtection"] if "EnableTerminationProtection" in stack else False
-    
+    response["EnableTerminationProtection"] = (
+        stack["EnableTerminationProtection"]
+        if "EnableTerminationProtection" in stack
+        else False
+    )
+
     tags = []
     if "Tags" in stack and stack["Tags"]:
         for key in stack["Tags"]:
             tags.append({"Key": key, "Value": stack["Tags"][key]})
     response["Tags"] = tags
 
-    return response 
+    return response
+
 
 def handle_stack(stack):
     stack_config = collect_stack_config(stack)
@@ -44,19 +48,39 @@ def handle_stack(stack):
         response = client.describe_stacks(StackName=stack["StackName"])
     except Exception as e:
         logging.warning(
-            f'Stack {stack["StackName"]} does not exist or invalid stack name')
-        response = client.create_stack(StackName=stack["StackName"], TemplateBody=stack["TemplateFile"], Parameters=stack_config["Parameters"],
-                                       Capabilities=stack["Capabilities"], Tags=stack_config["Tags"], EnableTerminationProtection=stack_config["EnableTerminationProtection"])
+            f'Stack {stack["StackName"]} does not exist or invalid stack name'
+        )
+        response = client.create_stack(
+            StackName=stack["StackName"],
+            TemplateBody=stack["TemplateFile"],
+            Parameters=stack_config["Parameters"],
+            Capabilities=stack["Capabilities"],
+            Tags=stack_config["Tags"],
+            EnableTerminationProtection=stack_config["EnableTerminationProtection"],
+        )
     else:
-        if response["StackStatus"] in ["CREATE_COMPLETE", "UPDATE_COMPLETE", "UPDATE_ROLLBACK_COMPLETE"]:
-            if 'Delete' in stack and stack['Delete']:
+        if response["StackStatus"] in [
+            "CREATE_COMPLETE",
+            "UPDATE_COMPLETE",
+            "UPDATE_ROLLBACK_COMPLETE",
+        ]:
+            if "Delete" in stack and stack["Delete"]:
                 client.delete_stack(StackName=stack["StackName"])
             else:
-                response = client.update_stack(StackName=stack["StackName"], TemplateBody=stack["TemplateFile"], Parameters=stack_config["Parameters"],
-                                       Capabilities=stack["Capabilities"], Tags=stack_config["Tags"], EnableTerminationProtection=stack_config["EnableTerminationProtection"])
+                response = client.update_stack(
+                    StackName=stack["StackName"],
+                    TemplateBody=stack["TemplateFile"],
+                    Parameters=stack_config["Parameters"],
+                    Capabilities=stack["Capabilities"],
+                    Tags=stack_config["Tags"],
+                    EnableTerminationProtection=stack_config[
+                        "EnableTerminationProtection"
+                    ],
+                )
+
 
 if __name__ == "__main__":
-    with open('manifest.yaml') as f:
+    with open("manifest.yaml") as f:
         stacks = yaml.safe_load(f)
         for stack in stacks["Stacks"]:
             print(stack)
